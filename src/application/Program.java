ba54a1e9fc3e16e6e24ab08f02f03270915e558a
Josevan Oliveira <josivantarcio@msn.com>
2024-07-31 13:21:02 -0300

    sellerDaoJDBC and daoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.time.LocalDate;
 import java.util.Locale;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,9 +13,11 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Department depto = new Department(1, "IT");
-		
+
 		Seller seller = new Seller(1, "josevan", "josevan@email.com", LocalDate.now(), 8000.00, depto);
-		
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+
 		System.out.println(seller);
 
 	}
