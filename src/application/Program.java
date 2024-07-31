aa58e8785e4c1c7943c5441f1826e6007927891a
Josevan Oliveira <josivantarcio@msn.com>
2024-07-31 14:49:07 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,21 @@
 package application;
 
-import java.time.LocalDate;
 import java.util.Locale;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
-		Department depto = new Department(1, "IT");
-
-		Seller seller = new Seller(1, "josevan", "josevan@email.com", LocalDate.now(), 8000.00, depto);
-
+		
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-
-		System.out.println(seller);
+		
+		Seller vendedor = sellerDao.findById(13);
+		System.out.println(vendedor);
 
 	}
 
